e07f398fb1863618ce7708a37d80e4e654950b5e
zhanggang027 <zhanggang027@ke.com>
2024-11-25 22:09:39 +0800

    chapter5

---
+++ language/chapter5/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	f(3)
+}
+func f(x int) {
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Printf("internal error: %v\n", p)
+		}
+	}()
+	fmt.Printf("f(%d)\n", x+0/x) // panics if x == 0
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
+}
